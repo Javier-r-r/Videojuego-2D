98fe6e16b5066b66d229724df6eb03836e4e712a
Javier Rodríguez Rodríguez <javier.2003@mundo-r.com>
2025-12-01 19:15:42 +0100

    Add files via upload

---
+++ assets/levels/tilesets/tsx/14_Swimming_Pool_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="14_Swimming_Pool_32x32" tilewidth="32" tileheight="32" tilecount="992" columns="32">
+ <image source="../png/14_Swimming_Pool_32x32.png" width="1024" height="992"/>
+</tileset>
